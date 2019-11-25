9f4d07c97b3faac18a0f362b09b03363d414a905
Samuel Machado <samuelmachado@MacBook-Air-de-Samuel.local>
2019-11-25 18:30:10 -0300

    socket implementado

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/chat2", function(req, res, next) {
+  res.render("chat2", { title: "Chat" });
+});
+
+router.get("/chat", function(req, res, next) {
+  res.render("chat", { title: "Express" });
 });
 
 module.exports = router;
